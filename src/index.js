fa3630e98872b537bbf3e9e6808c30020c1e54a9
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-10-08 20:48:20 +0100

    Change parser to node-podcast-parser

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-import {EventEmitter} from 'events';
-import parser from 'parse-rss';
+import { EventEmitter } from "events";
+import * as parsePodcast from "node-podcast-parser";
 
 function request(feedUrl) {
   return new Promise((resolve, reject) => {
-    parser(feedUrl, (error, entries) => {
-      if (error) { return reject(error); }
-      if (!entries) { return reject(new Error('No entries were founded. Check your url.')); }
+    parsePodcast(feedUrl, (error, entries) => {
+      if (error) {
+        return reject(error);
+      }
+      if (!entries) {
+        return reject(new Error("No entries were founded. Check your url."));
+      }
 
       // Sort by release.
-      entries.sort((ent1, ent2) => ((ent2.pubDate / 1000) - (ent1.pubDate / 1000)));
+      entries.sort((ent1, ent2) => ent2.pubDate / 1000 - ent1.pubDate / 1000);
       resolve(entries);
     });
   });
 }
-
 
 class Watcher extends EventEmitter {
   constructor(feedUrl, interval) {
     super();
 
     // Make sure the url exists and it's a string.
-    if (!feedUrl || typeof feedUrl !== 'string') {
-      throw new Error('feedUrl isn\'t defined');
+    if (!feedUrl || typeof feedUrl !== "string") {
+      throw new Error("feedUrl isn't defined");
     }
 
     this.feedUrl = feedUrl;
-    this.interval = interval || 60;  // 1 hour by default
+    this.interval = interval || 60; // 1 hour by default
     this.lastEntryDate = null; // Used to make sure if there is new entries.
     this.lastEntryTitle = null; // Used to avoid duplicates.
     this.timer = null; // Stores watcher function.
@@ -46,13 +49,13 @@ class Watcher extends EventEmitter {
   start() {
     return new Promise((resolve, reject) => {
       this.checkAll()
-        .then((entries) => {
+        .then(entries => {
           this.lastEntryDate = entries[0].pubDate / 1000;
           this.lastEntryTitle = entries[0].title;
           this.timer = this.watch();
           resolve(entries);
         })
-        .catch((err) => {
+        .catch(err => {
           reject(err);
         });
     });
@@ -61,17 +64,20 @@ class Watcher extends EventEmitter {
   // Stop watching.
   stop() {
     clearInterval(this.timer);
-    this.emit('stop');
+    this.emit("stop");
   }
 
   // Check the feed.
   watch() {
     const fetch = () => {
       this.checkAll()
-        .then((entries) => {
+        .then(entries => {
           // Filter older entries.
-          const newEntries = entries.filter((entry) => {
-            return (this.lastEntryDate === null || this.lastEntryDate < (entry.pubDate / 1000));
+          const newEntries = entries.filter(entry => {
+            return (
+              this.lastEntryDate === null ||
+              this.lastEntryDate < entry.pubDate / 1000
+            );
           });
 
           // Update last entry.
@@ -79,10 +85,10 @@ class Watcher extends EventEmitter {
           if (newEntries.length > 0) {
             this.lastEntryDate = newEntries[0].pubDate / 1000;
             this.lastEntryTitle = newEntries[0].title;
-            this.emit('new entries', newEntries);
+            this.emit("new entries", newEntries);
           }
         })
-        .catch(error => this.emit('error', error));
+        .catch(error => this.emit("error", error));
     };
 
     // Keep checking every n minutes.
